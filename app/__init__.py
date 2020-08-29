0796ee58d7f0e723a88ef6f747764aaa3cdfbdbd
Brian Choi <choi.brian.97@gmail.com>
2020-08-29 01:34:31 -0700

    auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -16,13 +16,16 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-login.login_view = 'login'
+login.login_view = 'auth.login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
+
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 from app import routes, models
 
