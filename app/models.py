905921580536076148a7cfd877a7bcf382c95942
Brian Choi <choi.brian.97@gmail.com>
2020-09-03 23:23:22 -0700

    Add search.py

--- app/models.py
+++ app/models.py
@@ -92,5 +92,7 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    __searchable__ = ['body']
+
     def __repr__(self):
         return '<Post {}>'.format(self.body)
